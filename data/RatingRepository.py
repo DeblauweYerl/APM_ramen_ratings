c7c6c8d2f19153a80c143f158b41f5a5e09d8e8f
YerlD <yerl.deblauwe@student.howest.be>
2021-04-08 18:16:37 +0200

    datarepository

--- data/RatingRepository.py
+++ data/RatingRepository.py
@@ -6,31 +6,25 @@ import pandas as pd
 from APM_ramen_ratings.models.Rating import Rating
 
 
+def read_ratings():
+    ratings = pd.read_csv("../assets/ramen-ratings.csv")
+    ratings = ratings[ratings['Stars'] != 'Unrated']
+    return ratings.values
+
+
 class RatingRepository:
     def __init__(self):
-        self.ratings = self.read_ratings()
-
-    def read_ratings(self):
-        # ratings = []
-        # with open('../assets/ramen-ratings.csv', newline='', encoding='UTF-8') as csvfile:
-        #     spamreader = csv.reader(csvfile, delimiter=',')
-        #     next(spamreader)
-        #     for row in spamreader:
-        #         if row[5] != 'Unrated':
-        #             ratings.append(Rating(row[0], row[1], row[2], row[3], row[4], row[5]))
-        ratings = pd.read_csv("../assets/ramen-ratings.csv")
-        print(ratings.values)
-        return ratings.values
+        self.ratings = read_ratings()
 
     def get_brands(self):
         brands = []
         [brands.append(rt[1]) for rt in self.ratings if rt[1] not in brands]
-        return brands
+        return sorted(brands)
 
     def get_countries(self):
         countries = []
         [countries.append(rt[4]) for rt in self.ratings if rt[4] not in countries]
-        return countries
+        return sorted(countries)
 
     def get_filtered_ratings(self, filters):
         response = self.ratings
@@ -42,12 +36,13 @@ class RatingRepository:
             response = [rt for rt in response if float(rt[5]) >= filters['min_rating']]
         return response
 
-    def get_mean_ratings_brand(self, brand, country):
-        return round(statistics.mean([float(rt.rating) for rt in self.ratings if rt[1] == brand and rt[4] == country]),2)
+    def get_mean_brand_rating(self, brand, country):
+        brands = self.get_brands()
+        {rt[1]:  for rt[rt for rt in self.get_filtered_ratings([br for br in self.get_brands()])]}
+        return round(statistics.mean([float(rt.rating) for rt in self.ratings if rt[1] == brand and rt[4] == country]), 2)
 
     def search_ratings(self, search):
-        return [rt.variety for rt in self.ratings if search in rt.variety]
-
+        return [rt for rt in self.ratings if search.lower() in rt[2].lower()]
 
 # ratings = RatingRepository()
 # print(ratings.get_brands())
