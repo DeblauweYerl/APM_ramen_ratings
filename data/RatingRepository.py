1da581635c6cd25cee5dc9c62d047c1387752b1b
Yerl <yerl.deblauwe@student.howest.be>
2021-04-09 09:30:38 +0200

    datarepository mean_ratings

--- data/RatingRepository.py
+++ data/RatingRepository.py
@@ -1,6 +1,8 @@
 import csv
 import statistics
 import json
+
+import numpy as np
 import pandas as pd
 
 from APM_ramen_ratings.models.Rating import Rating
@@ -36,15 +38,31 @@ class RatingRepository:
             response = [rt for rt in response if float(rt[5]) >= filters['min_rating']]
         return response
 
-    def get_mean_brand_rating(self, brand, country):
+    def get_mean_brand_rating(self):
         brands = self.get_brands()
-        {rt[1]:  for rt[rt for rt in self.get_filtered_ratings([br for br in self.get_brands()])]}
-        return round(statistics.mean([float(rt.rating) for rt in self.ratings if rt[1] == brand and rt[4] == country]), 2)
+        filters = {'filters':
+                       { 'brand': 'brand',
+                         'country': 'country',
+                         'min_rating': 0
+                       }}
+        avg_ratings = {}
+        for brand in brands:
+            filters['filters']['brand'] = brand
+            ratings = self.get_filtered_ratings(filters)
+            sumratings = 0
+            sumratings = [sumratings + rt[5] for rt in ratings]
+            avg_ratings[brand] = round(np.mean(sumratings), 2)
+        return avg_ratings
+
+        # kaka = [rt for rt in self.get_filtered_ratings({'filters': { 'brand': [br for br in self.get_brands()]), 'country': 'country', 'min_rating': 0}}]
+        print(kaka)
+        # return round(statistics.mean([float(rt.rating) for rt in self.ratings if rt[1] == brand and rt[4] == country]), 2)
 
     def search_ratings(self, search):
         return [rt for rt in self.ratings if search.lower() in rt[2].lower()]
 
-# ratings = RatingRepository()
+ratings = RatingRepository()
+ratings.get_mean_brand_rating()
 # print(ratings.get_brands())
 # print(ratings.get_countries())
 # print([rt[5] for rt in ratings.get_filtered_ratings({'brand': 'Nissin', 'country': 'Japan', 'min_rating': 3.75})])
